36ac9c4faa8bd34b95c4b44375f4daff68ec07f5
Rahul Ghosh <ghrahul2016@gmail.com>
2018-04-11 20:36:40 +0530

    creating country modelto show data in restapi

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=20)
+    capital = models.CharField(max_length=20)
+    president = models.CharField(max_length = 20)
+    
