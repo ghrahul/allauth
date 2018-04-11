01f3b7e5c6974ac33d311c96fa83b2324d19b1f3
Rahul Ghosh <ghrahul2016@gmail.com>
2018-04-11 20:53:22 +0530

    creating a getview using apiview and defining the url

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Country
+from .serialzers import CountrySerializer
 # Create your views here.
+
+class Countrylist(APIView):
+    def get(self,request):
+        country = Country.objects.all()
+        serializer = CountrySerializer(country,many=True)
+        return Response(serializer)
+        
--- project/project/urls.py
+++ project/project/urls.py
@@ -18,9 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from rest_framework.urlpatterns import format_suffix_patterns
+from app import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^country/', views.Countrylist.as_view()),
+
 
 ]
